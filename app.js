0997d4f68e08fac2c43028fec9cab159aba98280
Reljadin <rivanovic10120rn@raf.rs>
2021-12-29 21:03:32 +0100

    rade veze

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const { MissionThreads} = require('./models');
 const path = require("path");
 
 const Joi = require('joi');
+const soldiers = require("./models/soldiers");
 
 const app = express();
 
@@ -18,11 +19,25 @@ async function main(){
     let usr;
     let msg;
 
-    usr = await Soldiers.create({ name: 'Brother-Captain Thybalt', tag:'86-100-6', role:'HQ', SquadID:'1', LoadoutID:'1', status: 'Missing', password:'1800ontheblock' });
-    console.log(JSON.stringify(usr));
+    // usr = await Soldiers.create({ name: 'Brother-Captain Thybalt', tag:'86-100-6', role:'HQ', SquadID:'1', LoadoutID:'1', status: 'Missing', password:'1800ontheblock' });
+    // console.log(JSON.stringify(usr));
 
-    usr = await Soldiers.findOne({where:{id: 1}});
-    console.log(JSON.stringify(usr));
+    // usr = await Soldiers.findOne({where:{id: 1}});
+    // console.log(JSON.stringify(usr));
+
+    // usr = await Soldiers.findAll();
+    // console.log(JSON.stringify(usr));
+
+    // usr = await Loadouts.findAll();
+    // console.log(JSON.stringify(usr));
+
+    // usr = await Squads.findAll();
+    // console.log(JSON.stringify(usr));
+
+    msg = await Soldiers.findAll({include: ['equippedWith']})
+    console.log(msg[0].tag);
+
+    console.log(msg[0].equippedWith.meleeWeapon);
 
     await sequelize.close();
 
