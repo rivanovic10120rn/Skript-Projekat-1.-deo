ab4dbff34716196ac7208df8d2563ca109c211d3
Reljadin <rivanovic10120rn@raf.rs>
2022-01-08 05:38:47 +0100

    fghiul fsa

--- static/squads/squads.js
+++ static/squads/squads.js
@@ -1,17 +1,17 @@
 function init() {
     
-    fetch('http://localhost:8080/admin/squads/')
-        .then( res => res.json() )
-        .then( data => {
-            const lst = document.getElementById('squadList');
+    // fetch('http://localhost:8080/admin/squads/')
+    //     .then( res => res.json() )
+    //     .then( data => {
+    //         const lst = document.getElementById('squadList');
 
-            data.forEach( el => {
-                lst.innerHTML += `<li>ID: ${el.id}</li> <ul><li>Name: ${el.name}</li> <li>Type: ${el.type}</li> <li>Status: ${el.status}</li></ul>`;
-            });
-        });
+    //         data.forEach( el => {
+    //             lst.innerHTML += `<li>ID: ${el.id}</li> <ul><li>Name: ${el.name}</li> <li>Type: ${el.type}</li> <li>Status: ${el.status}</li></ul>`;
+    //         });
+    //     });
 
 
-    fetch('http://localhost:8080/admin/squads/')
+    fetch('http://localhost:8080/admin/squads')
         .then( res => res.json() )
         .then( data => {
             const lst = document.getElementById('squadTable');
@@ -20,58 +20,29 @@ function init() {
                 lst.innerHTML += `<tr><td>${el.id}</td> <td>${el.name}</td> <td>${el.type}</td> <td>${el.status}</td></tr>`;
             });
         });
-
-    fetch('http://localhost:8080/admin/missionthreads')
-        .then( res => res.json() )
-        .then( data => {
-            const lst = document.getElementById('msgLst');
-
-            data.forEach( el => {
-                lst.innerHTML += `<li>ID: ${el.id}\n Body: ${el.body}\n User: ${el.user.name}</li>`;
-            });
-        });
     
-    document.getElementById('usrBtn').addEventListener('click', e => {
+    document.getElementById('squadsButton').addEventListener('click', e => {
         e.preventDefault();
 
         const data = {
             name: document.getElementById('name').value,
-            email: document.getElementById('email').value
+            type: document.getElementById('type').value,
+            status: document.getElementById('status').value
         };
 
         document.getElementById('name').value = '';
-        document.getElementById('email').value = '';
+        document.getElementById('type').value = '';
+        document.getElementById('status').value = '';
 
-        fetch('http://192.168.0.143:8000/api/users', {
+        fetch('http://localhost:8080/admin/squads', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(data)
         })
             .then( res => res.json() )
             .then( data => {
-                document.getElementById('usrLst').innerHTML += `<li>ID: ${data.id}, Name: ${data.name}, E-mail: ${data.email}</li>`;
+                document.getElementById('squadTable').innerHTML += `<tr><td>${data.id}</td> <td>${data.name}</td> <td>${data.type}</td> <td>${data.status}</td></tr>`;
             });
     });
 
-    document.getElementById('msgBtn').addEventListener('click', e => {
-        e.preventDefault();
-        
-        const data = {
-            body: document.getElementById('body').value,
-            userId: document.getElementById('userId').value
-        };
-
-        document.getElementById('body').value = '';
-        document.getElementById('userId').value = '';
-
-        fetch('http://192.168.0.143:8000/api/messages', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(data)
-        })
-            .then( res => res.json() )
-            .then( data => {
-                document.getElementById('msgLst').innerHTML += `<li>ID: ${data.id}, Body: ${data.body}</li>`;
-            });
-    });
 }
