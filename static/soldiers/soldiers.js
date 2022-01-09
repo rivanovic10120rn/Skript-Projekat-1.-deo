192c548d3cfd15c73bc1731ba2685c6d087b8ac4
Reljadin <rivanovic10120rn@raf.rs>
2022-01-09 05:06:23 +0100

    eksperimentalne get rade

--- static/soldiers/soldiers.js
+++ static/soldiers/soldiers.js
@@ -33,4 +33,46 @@ function init() {
                 }});
     });
 
+    document.getElementById('soldierSquadButton').addEventListener('click', e => {
+        e.preventDefault();
+
+        const url = document.getElementById('SquadID').value;
+
+        document.getElementById('soldierTable').innerHTML='';
+        document.getElementById('SquadID').value ='';
+
+        fetch('http://localhost:8080/admin/soldiers/squad/' + url,)
+            .then( res => res.json() )
+            .then( data => {
+                const lst = document.getElementById('soldierTable');
+
+                if(data.msg){
+                    alert(data.msg)
+                }else{
+                data.forEach( el => {
+                    lst.innerHTML += `<tr><td>${el.id}</td> <td>${el.name}</td> <td>${el.tag}</td> <td>${el.role}</td> <td>${el.SquadID}</td> <td>${el.LoadoutID}</td> <td>${el.password}</td> <td>${el.status}</td></tr>`;
+            })}});
+    });
+
+    document.getElementById('soldierLoadoutButton').addEventListener('click', e => {
+        e.preventDefault();
+
+        const LoadoutID = document.getElementById('LoadoutID').value;
+
+        document.getElementById('soldierTable').innerHTML='';
+        document.getElementById('LoadoutID').value ='';
+
+        fetch('http://localhost:8080/admin/soldiers/loadout/' + LoadoutID,)
+            .then( res => res.json() )
+            .then( data => {
+                const lst = document.getElementById('soldierTable');
+
+                if(data.msg){
+                    alert(data.msg)
+                }else{
+                data.forEach( el => {
+                    lst.innerHTML += `<tr><td>${el.id}</td> <td>${el.name}</td> <td>${el.tag}</td> <td>${el.role}</td> <td>${el.SquadID}</td> <td>${el.LoadoutID}</td> <td>${el.password}</td> <td>${el.status}</td></tr>`;
+            })}});
+    });
+
 }
