309770869e08dd4d75b86ecc98dde433671b25f2
Reljadin <rivanovic10120rn@raf.rs>
2022-01-09 07:22:08 +0100

    osnovni gui gotov - fali jos samo autorizacija ili sta god

---
+++ static/missionthreads/deleteMissionThread.js
@@ -0,0 +1,48 @@
+function init() {
+
+    fetch('http://localhost:8080/admin/missionthreads')
+        .then( res => res.json() )
+        .then( data => {
+            const lst = document.getElementById('missionThreadTable');
+
+            if(data.msg){
+                alert(data.msg);
+            }else{
+            data.forEach( el => {
+                lst.innerHTML += `<tr><td>${el.id}</td> <td>${el.message}</td> <td>${el.MissionID}</td> <td>${el.SoldierID}</td> <td>${el.recieved}</td> <td>${el.createdAt}</td> <td>${el.updatedAt}</td></tr>`;
+        });
+    }});
+
+    document.getElementById('missionThreadDeleteButton').addEventListener('click', e => {
+        e.preventDefault();
+
+        const id = document.getElementById('id').value;
+
+        document.getElementById('missionThreadTable').innerHTML='';
+        document.getElementById('id').value='';
+
+        fetch('http://localhost:8080/admin/missionthreads/' +id, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' },
+        })
+            .then( res => res.json() )
+            .then( el => {
+                if(el.msg){
+                    alert(el.msg)
+                }else{
+                    fetch('http://localhost:8080/admin/missionthreads')
+                    .then( res => res.json() )
+                    .then( data => {
+                        const lst = document.getElementById('missionThreadTable');
+            
+                        if(data.msg){
+                            alert(data.msg);
+                        }else{
+                        data.forEach( el => {
+                            lst.innerHTML += `<tr><td>${el.id}</td> <td>${el.message}</td> <td>${el.MissionID}</td> <td>${el.SoldierID}</td> <td>${el.recieved}</td> <td>${el.createdAt}</td> <td>${el.updatedAt}</td></tr>`;
+                    });
+                }});
+            }});
+    });
+
+}
