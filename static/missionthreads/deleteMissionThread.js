be4cc6912553b9dd8b82ae1092973d9a18176f24
Reljadin <rivanovic10120rn@raf.rs>
2022-01-14 04:39:59 +0100

    mnogo hidden greskica pri dodavanju app_autha - pogotovo kod ruta

--- static/missionthreads/deleteMissionThread.js
+++ static/missionthreads/deleteMissionThread.js
@@ -25,6 +25,7 @@ function init() {
 
         const id = document.getElementById('id').value;
 
+        
         document.getElementById('missionThreadTable').innerHTML='';
         document.getElementById('id').value='';
 
@@ -37,6 +38,7 @@ function init() {
         })
             .then( res => res.json() )
             .then( el => {
+
                 if(el.msg){
                     alert(el.msg)
                 }else{
@@ -45,17 +47,17 @@ function init() {
                             'Authorization': `Bearer ${token}`
                         }
                     })
-                    .then( res => res.json() )
-                    .then( data => {
-                        const lst = document.getElementById('missionThreadTable');
-            
-                        if(data.msg){
-                            alert(data.msg);
-                        }else{
-                        data.forEach( el => {
-                            lst.innerHTML += `<tr><td>${el.id}</td> <td>${el.message}</td> <td>${el.MissionID}</td> <td>${el.SoldierID}</td> <td>${el.recieved}</td> <td>${el.createdAt}</td> <td>${el.updatedAt}</td></tr>`;
-                    });
-                }});
+                        .then( res => res.json() )
+                        .then( data => {
+                            const lst = document.getElementById('missionThreadTable');
+
+                            if(data.msg){
+                                alert(data.msg);
+                            }else{
+                            data.forEach( el => {
+                                lst.innerHTML += `<tr><td>${el.id}</td> <td>${el.message}</td> <td>${el.MissionID}</td> <td>${el.SoldierID}</td> <td>${el.recieved}</td> <td>${el.createdAt}</td> <td>${el.updatedAt}</td></tr>`;
+                        });
+                    }});
             }});
     });
 
